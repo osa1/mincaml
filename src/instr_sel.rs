351d13ed4e287aaa7aa4210e671ce47d8190b90f
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-05-09 17:14:53 +0300

    More documentation on instruction selection

--- src/instr_sel.rs
+++ src/instr_sel.rs
@@ -24,9 +24,50 @@
 //
 // - Set EFLAGS with ucomisd: "Unordered Compare Scalar Double-Precision Floating-Point Values"
 //
+// (Reference: https://www.felixcloutier.com/x86/ucomisd)
+//
 // Use the same branching instructions.
 //
 // Moving floats
 // -------------
 //
 // - MOVSD — Move or Merge Scalar Double-Precision Floating-Point Value
+//   Example: `movsd %xmm0, -32(%rbp)` to move a double to a struct field.
+//            `movsd -56(%rbp), %xmm0` the other way around
+//
+// Integer arithmetic
+// ------------------
+//
+// - addq/subq
+//
+// (Reference: gcc -S output)
+//
+// Float arithmetic
+// ----------------
+//
+// - addsd/subsd/divsd/mulsd
+//
+// Integer to double example: `cvtsi2sdq %rax, %xmm0` (NOTE: I don't think we need this tho?)
+//
+// (Reference: gcc -S output)
+//
+// Registers
+// ---------
+//
+// rsp and rbp are used for stack manipulation. Remaining args are used for parameter passing in
+// this order:
+//
+// - rax, rcx, rdx, rbx, rsi, rdi, r8, r9, r10, r11, r12, r13, r14, r15 (14 regs)
+// - For doubles: xmm0 - xmm7 (TODO: can we not use the rest?)
+//
+// All registers are callee-save.
+
+#[rustfmt::skip]
+enum WordReg {
+    RAX, RCX, RDX, RBX, RSI, RDI, RSP, RBP, R8, R9, R10, R11, R12, R13, R14, R15,
+}
+
+#[rustfmt::skip]
+enum DoubleReg {
+    XMM0, XMM1, XMM2, XMM3, XMM4, XMM5, XMM6, XMM7
+}
