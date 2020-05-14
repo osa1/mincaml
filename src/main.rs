d53147940c4ae24cdb98ebbfb19ae967754371ad
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-05-14 22:15:25 +0300

    Implement simple stats for passes

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ use parser::parse;
 use type_check::type_check_pgm;
 
 use std::process::exit;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 #[cfg(debug_assertions)]
 #[global_allocator]
@@ -48,20 +48,46 @@ fn main() {
     }
 }
 
+#[derive(Debug)]
+struct PassStats {
+    name: &'static str,
+    time: Duration,
+    allocs: usize,
+}
+
+fn record_pass_stats<A, F: FnOnce() -> A>(
+    stats: &mut Vec<PassStats>, pass_name: &'static str, pass: F,
+) -> A {
+    let allocs_before = perf::get_allocated();
+    let start_time = Instant::now();
+    let ret = pass();
+    let elapsed = start_time.elapsed();
+    let allocs_after = perf::get_allocated();
+    stats.push(PassStats {
+        name: pass_name,
+        time: elapsed,
+        allocs: allocs_after - allocs_before,
+    });
+    ret
+}
+
 fn do_expr(expr_str: &str) -> i32 {
-    let tokens: Vec<Token> = match tokenize(expr_str) {
-        Err(err) => {
-            println!("Lexer error: {:#?}", err);
-            return 1;
-        }
-        Ok(tokens) => tokens,
-    };
+    let mut pass_stats: Vec<PassStats> = Vec::with_capacity(10);
+
+    let tokens: Vec<Token> =
+        match record_pass_stats(&mut pass_stats, "tokenize", || tokenize(expr_str)) {
+            Err(err) => {
+                println!("Lexer error: {:#?}", err);
+                return 1;
+            }
+            Ok(tokens) => tokens,
+        };
 
     // println!("{:#?}", tokens);
 
     let mut ctx = Default::default();
 
-    let mut expr = match parse(&mut ctx, &tokens) {
+    let mut expr = match record_pass_stats(&mut pass_stats, "parse", || parse(&mut ctx, &tokens)) {
         Err(err) => {
             println!("Parser error: {:#?}", err);
             return 1;
@@ -71,7 +97,9 @@ fn do_expr(expr_str: &str) -> i32 {
 
     // println!("Expr: {:#?}", expr);
 
-    match type_check_pgm(&mut ctx, &mut expr) {
+    match record_pass_stats(&mut pass_stats, "type check", || {
+        type_check_pgm(&mut ctx, &mut expr)
+    }) {
         Err(err) => {
             println!("Type error: {:#?}", err);
             return 1;
@@ -81,12 +109,14 @@ fn do_expr(expr_str: &str) -> i32 {
 
     // println!("Type-checked expr: {:#?}", expr);
 
-    let expr = knormal(&mut ctx, expr);
+    let expr = record_pass_stats(&mut pass_stats, "knormal", || knormal(&mut ctx, expr));
 
     // println!("K normalized:");
     // println!("{:?}", expr);
 
-    let funs = closure_convert(&mut ctx, expr);
+    let funs = record_pass_stats(&mut pass_stats, "closure convert", || {
+        closure_convert(&mut ctx, expr)
+    });
 
     println!("### Closure conversion:\n");
 
@@ -99,31 +129,37 @@ fn do_expr(expr_str: &str) -> i32 {
 
     println!("### Code generation:\n");
 
-    codegen(&mut ctx, &funs);
+    record_pass_stats(&mut pass_stats, "codegen", || codegen(&mut ctx, &funs));
+
+    report_pass_stats(&pass_stats);
 
     0
 }
 
-fn do_file(file: &str) -> i32 {
-    let start_time = Instant::now();
-    perf::reset_allocated();
-
-    let contents = std::fs::read_to_string(file).unwrap();
-    let ret = do_expr(&contents);
-
-    let allocated = perf::get_allocated();
-    let elapsed = start_time.elapsed();
-
-    println!(
-        "File took {} ms to build.",
-        utils::comma_sep(&format!("{}", elapsed.as_millis()))
-    );
-    if allocated != 0 {
+fn report_pass_stats(pass_stats: &[PassStats]) {
+    println!("--------------------------------------------------------");
+    let mut total_elapsed: Duration = Duration::from_micros(0);
+    let mut total_allocated: usize = 0;
+    for PassStats { name, time, allocs } in pass_stats {
         println!(
-            "{} bytes allocated.",
-            utils::comma_sep(&format!("{}", allocated))
+            "{}: {} ms, {} bytes",
+            name,
+            time.as_millis(),
+            utils::comma_sep(&format!("{}", allocs))
         );
+        total_elapsed += *time;
+        total_allocated += allocs;
     }
 
-    ret
+    println!(
+        "TOTAL: {} ms, {} bytes",
+        total_elapsed.as_millis(),
+        utils::comma_sep(&format!("{}", total_allocated))
+    );
+    println!("--------------------------------------------------------");
+}
+
+fn do_file(file: &str) -> i32 {
+    let contents = std::fs::read_to_string(file).unwrap();
+    do_expr(&contents)
 }
