455731c8cf1b32142e9fcf95867c2a077d7062b3
Ömer Sinan Ağacan <omeragacan@gmail.com>
2021-05-04 20:43:59 +0300

    Replace hand-written lexer with lexgen

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,7 @@
 use std::str::FromStr;
 
+use lexgen::lexer;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Token {
     LParen,
@@ -35,351 +37,144 @@ pub enum Token {
     Float(f64),
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum LexErr {
-    EndOfInput,
-    // UnterminatedComment,
-    UnexpectedChar { expected: u8, found: u8 },
-    UnexpectedUppercaseChar { found: u8 },
     InvalidFloat { found: String },
     InvalidInt { found: String },
 }
 
-pub struct Lexer<'a> {
-    input: &'a [u8],
-    // Current position in `input`
-    byte_idx: usize,
-    buf: String,
+#[derive(Debug, Default)]
+pub struct LexerState {
+    comment_depth: usize,
 }
 
-static ARRAY_CREATE_STR: &str = "Array.create";
-static ARRAY_CREATE_LEN: usize = ARRAY_CREATE_STR.len();
-
-static ARRAY_MAKE_STR: &str = "Array.make";
-static ARRAY_MAKE_LEN: usize = ARRAY_MAKE_STR.len();
-
-pub fn tokenize(expr_str: &str) -> Result<Vec<Token>, LexErr> {
-    let mut lexer = Lexer::new(expr_str.as_bytes());
+pub fn tokenize(expr_str: &str) -> Result<Vec<Token>, LexerError> {
+    let lexer = Lexer::new(expr_str);
     let mut tokens = vec![];
-    loop {
-        match lexer.next() {
-            Err(LexErr::EndOfInput) => {
-                break;
-            }
-            Err(err) => {
-                return Err(err);
-            }
-            Ok(tok) => {
-                tokens.push(tok);
-            }
+    for next in lexer {
+        match next {
+            Ok(token) => tokens.push(token.1),
+            Err(err) => return Err(err),
         }
     }
     Ok(tokens)
 }
 
-impl<'a> Lexer<'a> {
-    pub fn new(input: &[u8]) -> Lexer {
-        Lexer {
-            input,
-            byte_idx: 0,
-            buf: String::with_capacity(20),
-        }
-    }
+lexer! {
+    pub Lexer(LexerState) -> Token;
 
-    pub fn next(&mut self) -> Result<Token, LexErr> {
-        loop {
-            match self.next_byte()? {
-                next if next.is_ascii_whitespace() => {
-                    self.consume();
-                }
-                next if next.is_ascii_digit() => {
-                    return self.expect_int_or_float();
-                }
-                b'(' => {
-                    self.consume();
-                    if let Ok(b'*') = self.next_byte() {
-                        self.consume();
-                        self.consume_comment()?;
-                    } else {
-                        return Ok(Token::LParen);
-                    }
-                }
-                b')' => {
-                    self.consume();
-                    return Ok(Token::RParen);
-                }
-                b'-' => {
-                    self.consume();
-                    if let Ok(b'.') = self.next_byte() {
-                        self.consume();
-                        return Ok(Token::MinusDot);
-                    } else {
-                        return Ok(Token::Minus);
-                    }
-                }
-                b'+' => {
-                    self.consume();
-                    if let Ok(b'.') = self.next_byte() {
-                        self.consume();
-                        return Ok(Token::PlusDot);
-                    } else {
-                        return Ok(Token::Plus);
-                    }
-                }
-                b'*' => {
-                    self.consume();
-                    self.expect_char(b'.')?;
-                    self.consume();
-                    return Ok(Token::AstDot);
-                }
-                b'/' => {
-                    self.consume();
-                    self.expect_char(b'.')?;
-                    self.consume();
-                    return Ok(Token::SlashDot);
-                }
-                b'=' => {
-                    self.consume();
-                    return Ok(Token::Equal);
-                }
-                b'<' => {
-                    self.consume();
-                    let next = self.next_byte();
-                    match next {
-                        Ok(b'>') => {
-                            self.consume();
-                            return Ok(Token::LessGreater);
-                        }
-                        Ok(b'=') => {
-                            self.consume();
-                            return Ok(Token::LessEqual);
-                        }
-                        Ok(b'-') => {
-                            self.consume();
-                            return Ok(Token::LessMinus);
-                        }
-                        _ => {
-                            return Ok(Token::Less);
-                        }
-                    }
-                }
-                b'>' => {
-                    self.consume();
-                    let next = self.next_byte();
-                    match next {
-                        Ok(b'=') => {
-                            self.consume();
-                            return Ok(Token::GreaterEqual);
-                        }
-                        _ => {
-                            return Ok(Token::Greater);
-                        }
-                    }
-                }
-                b'.' => {
-                    self.consume();
-                    return Ok(Token::Dot);
-                }
-                b',' => {
-                    self.consume();
-                    return Ok(Token::Comma);
-                }
-                b';' => {
-                    self.consume();
-                    return Ok(Token::Semicolon);
-                }
-                b'_' => {
-                    self.consume();
-                    return Ok(Token::Underscore);
-                }
-                _ => {
-                    return self.expect_kw_or_id();
-                }
-            }
-        }
-    }
+    type Error = LexErr;
 
-    fn expect_kw_or_id(&mut self) -> Result<Token, LexErr> {
-        let next = self.next_byte()?;
-        if next.is_ascii_lowercase() {
-            self.consume();
-            self.buf.push(char::from(next));
-            loop {
-                let next = self.next_byte();
-                match next {
-                    Err(_) => {
-                        return Ok(self.process_id());
-                    }
-                    Ok(next) => {
-                        if next.is_ascii_digit()
-                            || next.is_ascii_lowercase()
-                            || next.is_ascii_uppercase()
-                            || next == b'_'
-                        {
-                            self.consume();
-                            self.buf.push(char::from(next));
-                        } else {
-                            return Ok(self.process_id());
-                        }
-                    }
+    rule Init {
+        [' ' '\t' '\n']+,
+
+        "(" = Token::LParen,
+        ")" = Token::RParen,
+        "true" = Token::Bool(true),
+        "false" = Token::Bool(false),
+        "not" = Token::Not,
+        "if" = Token::If,
+        "then" = Token::Then,
+        "else" = Token::Else,
+        "let" = Token::Let,
+        "rec" = Token::Rec,
+        "in" = Token::In,
+        "-" = Token::Minus,
+        "-." = Token::MinusDot,
+        "+" = Token::Plus,
+        "+." = Token::PlusDot,
+        "*." = Token::AstDot,
+        "/." = Token::SlashDot,
+        "=" = Token::Equal,
+        "<>" = Token::LessGreater,
+        "<=" = Token::LessEqual,
+        "<-" = Token::LessMinus,
+        "<" = Token::Less,
+        ">=" = Token::GreaterEqual,
+        ">" = Token::Greater,
+        "." = Token::Dot,
+        "," = Token::Comma,
+        ";" = Token::Semicolon,
+        "_" = Token::Underscore,
+        "Array.create" = Token::ArrayCreate,
+        "Array.make" = Token::ArrayCreate,
+
+        ['a'-'z'] ['a'-'z' 'A'-'Z' '_' '0'-'9']* =>
+            |lexer| {
+                let match_ = lexer.match_();
+                lexer.return_(Token::Id(match_.to_owned()))
+            },
+
+        ['0'-'9']+ =?
+            |lexer| {
+                let match_ = lexer.match_();
+                match i64::from_str(match_) {
+                    Ok(i) => lexer.return_(Ok(Token::Int(i))),
+                    Err(_) => lexer.return_(Err(LexErr::InvalidInt { found: match_.to_owned() })),
                 }
-            }
-        } else {
-            // Array.create or Array.make
-            let rest = &self.input[self.byte_idx..];
-            if rest.len() >= ARRAY_CREATE_LEN
-                && &rest[..ARRAY_CREATE_LEN] == ARRAY_CREATE_STR.as_bytes()
-            {
-                self.byte_idx += ARRAY_CREATE_LEN;
-                Ok(Token::ArrayCreate)
-            } else if rest.len() >= ARRAY_MAKE_LEN
-                && &rest[..ARRAY_MAKE_LEN] == ARRAY_MAKE_STR.as_bytes()
-            {
-                self.byte_idx += ARRAY_MAKE_LEN;
-                Ok(Token::ArrayCreate)
-            } else {
-                Err(LexErr::UnexpectedUppercaseChar { found: next })
-            }
-        }
-    }
+            },
 
-    fn process_id(&mut self) -> Token {
-        debug_assert!(!self.buf.is_empty());
-        let id = self.flush_buf();
-        match id.as_str() {
-            "if" => Token::If,
-            "then" => Token::Then,
-            "else" => Token::Else,
-            "let" => Token::Let,
-            "in" => Token::In,
-            "rec" => Token::Rec,
-            "true" => Token::Bool(true),
-            "false" => Token::Bool(false),
-            "not" => Token::Not,
-            _ => Token::Id(id),
-        }
-    }
-
-    fn expect_int_or_float(&mut self) -> Result<Token, LexErr> {
-        let mut dot_seen = false;
-        loop {
-            let next = self.next_byte();
-            match next {
-                Err(_) => {
-                    break;
+        ['0'-'9']+ '.' ['0'-'9']* =?
+            |lexer| {
+                let match_ = lexer.match_();
+                match f64::from_str(match_) {
+                    Ok(f) => lexer.return_(Ok(Token::Float(f))),
+                    Err(_) => lexer.return_(Err(LexErr::InvalidFloat { found: match_.to_owned() })),
                 }
-                Ok(b'.') => {
-                    if dot_seen {
-                        let invalid = self.flush_buf();
-                        return Err(LexErr::InvalidFloat { found: invalid });
-                    } else {
-                        self.buf.push('.');
-                        self.consume();
-                        dot_seen = true;
-                    }
+            },
+
+        "(*" =>
+            |mut lexer| {
+                lexer.state().comment_depth = 1;
+                lexer.switch(LexerRule::Comment)
+            },
+    },
+
+    rule Comment {
+        "(*" =>
+            |mut lexer| {
+                let depth = &mut lexer.state().comment_depth;
+                *depth =  *depth + 1;
+                lexer.continue_()
+            },
+
+        "*)" =>
+            |mut lexer| {
+                let depth = &mut lexer.state().comment_depth;
+                if *depth == 1 {
+                    lexer.switch(LexerRule::Init)
+                } else {
+                    *depth = *depth - 1;
+                    lexer.continue_()
                 }
-                Ok(next) if next.is_ascii_digit() => {
-                    self.consume();
-                    self.buf.push(char::from(next));
-                }
-                _ => {
-                    break;
-                }
-            }
-        }
+            },
 
-        if dot_seen {
-            self.process_float()
-        } else {
-            self.process_int()
-        }
-    }
+        // TODO: Having to add these too rules is a bit counter intuitive. Maybe
+        // consider this a lexgen bug?
+        '*',
+        '(',
 
-    fn process_float(&mut self) -> Result<Token, LexErr> {
-        let str = self.flush_buf();
-        match f64::from_str(&str) {
-            Ok(f) => Ok(Token::Float(f)),
-            Err(_) => Err(LexErr::InvalidFloat { found: str }),
-        }
-    }
+        _,
+    },
+}
 
-    fn process_int(&mut self) -> Result<Token, LexErr> {
-        let str = self.flush_buf();
-        match i64::from_str(&str) {
-            Ok(i) => Ok(Token::Int(i)),
-            Err(_) => Err(LexErr::InvalidInt { found: str }),
-        }
-    }
-
-    fn expect_char(&mut self, char: u8) -> Result<(), LexErr> {
-        let next = self.next_byte()?;
-        if next == char {
-            Ok(())
-        } else {
-            Err(LexErr::UnexpectedChar {
-                expected: char,
-                found: next,
-            })
-        }
-    }
-
-    fn consume_comment(&mut self) -> Result<(), LexErr> {
-        let mut level = 1;
-        while level != 0 {
-            match self.next_byte()? {
-                b'(' => {
-                    self.consume();
-                    if let Ok(b'*') = self.next_byte() {
-                        self.consume();
-                        level += 1;
-                    }
-                }
-                b'*' => {
-                    self.consume();
-                    if let Ok(b')') = self.next_byte() {
-                        self.consume();
-                        level -= 1;
-                    }
-                }
-                _ => {
-                    self.consume();
-                }
-            }
-        }
-        Ok(())
-    }
-
-    fn next_byte(&self) -> Result<u8, LexErr> {
-        match self.input.get(self.byte_idx) {
-            None => Err(LexErr::EndOfInput),
-            Some(next) => Ok(*next),
-        }
-    }
-
-    fn consume(&mut self) {
-        self.byte_idx += 1;
-    }
-
-    fn flush_buf(&mut self) -> String {
-        ::std::mem::replace(&mut self.buf, String::with_capacity(20))
-    }
+#[cfg(test)]
+fn unwrap_ignore_pos<T, E: std::fmt::Debug>(token: Option<Result<(usize, T, usize), E>>) -> T {
+    token.unwrap().unwrap().1
 }
 
 #[test]
 fn lexer_test() {
-    let input = "(* blah blah *) > < <> = +. - +";
-    let mut lexer = Lexer::new(input.as_bytes());
-    assert_eq!(lexer.next().unwrap(), Token::Greater);
-    assert_eq!(lexer.next().unwrap(), Token::Less);
-    assert_eq!(lexer.next().unwrap(), Token::LessGreater);
-    assert_eq!(lexer.next().unwrap(), Token::Equal);
-    assert_eq!(lexer.next().unwrap(), Token::PlusDot);
-    assert_eq!(lexer.next().unwrap(), Token::Minus);
-    assert_eq!(lexer.next().unwrap(), Token::Plus);
-    match lexer.next() {
-        Err(LexErr::EndOfInput) => {}
-        other => {
-            panic!("{:#?}", other);
-        }
-    }
+    let input = "(* test *) (* (* (* ** *) *) *) > < <> = +. - + let rec";
+    let mut lexer = Lexer::new(input);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Greater);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Less);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::LessGreater);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Equal);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::PlusDot);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Minus);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Plus);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Let);
+    assert_eq!(unwrap_ignore_pos(lexer.next()), Token::Rec);
+    assert_eq!(lexer.next(), None);
 }
