1b3abbeca1249c6aa161ba5a471491771cb23da4
Ömer Sinan Ağacan <omeragacan@gmail.com>
2022-08-19 09:02:43 +0200

    Bump Rust edition, document a few things

--- src/lower/types.rs
+++ src/lower/types.rs
@@ -8,18 +8,30 @@ use cranelift_entity::{entity_impl, PrimaryMap};
 pub struct BlockIdx(u32);
 entity_impl!(BlockIdx, "b");
 
-// Functions
+/// A function
 #[derive(Debug)]
 pub struct Fun {
+    /// Name of the function
     pub name: VarId,
+
+    /// Argument names of the function
     pub args: Vec<VarId>,
+
+    /// Basic blocks of the function. `BlockIdx(0)` is the entry block.
     pub blocks: PrimaryMap<BlockIdx, BlockData>,
+
+    /// Return type of the function
     pub return_type: RepType,
 }
 
+/// Body of a basic block. The basic block may be in construction, in which case the body will be
+/// `BlockData::NA`.
 #[derive(Debug)]
 pub enum BlockData {
+    /// Placeholder for a basic block currently being constructed
     NA,
+
+    /// Body of a basic block
     Block(Block),
 }
 
@@ -40,29 +52,41 @@ impl BlockData {
     }
 }
 
-// Basic blocks
+/// A basic block
 #[derive(Debug)]
 pub struct Block {
+    /// Index of the basic block
     pub idx: BlockIdx,
+
+    /// Any comments attached to the basic block. Debug printers for the basic block will print
+    /// this string to help with debugging.
     pub comment: Option<String>,
+
+    /// Statements in the basic block
     pub stmts: Vec<Stmt>,
+
+    /// Exit node of the basic block
     pub exit: Exit,
 }
 
+// TODO: I don't remember why we have this distinction?
 #[derive(Debug)]
 pub enum Stmt {
     Asgn(Asgn),
     Expr(Expr),
 }
 
-// Assignments
+/// An assignment
 #[derive(Debug)]
 pub struct Asgn {
+    /// Left-hand side of the assignment
     pub lhs: VarId,
+
+    /// Right-hand side of the assignment
     pub rhs: Expr,
 }
 
-// Assignment right-hand sides
+/// An assignment right-hand side
 #[derive(Debug)]
 pub enum Expr {
     Atom(Atom),
@@ -70,18 +94,30 @@ pub enum Expr {
     FBinOp(BinOp<FloatBinOp>),
     Neg(VarId),
     FNeg(VarId),
+
+    /// Function application. `RepType` is the runtime representation of the return value.
     App(VarId, Vec<VarId>, RepType),
-    // Tuple allocation
-    Tuple { len: usize },
-    // Tuple field read
+
+    /// Tuple allocation
+    Tuple {
+        len: usize,
+    },
+
+    /// Tuple field read
     TupleGet(VarId, usize),
-    // Tuple field write
+
+    /// Tuple field write
     TuplePut(VarId, usize, VarId),
-    // Array allocation
-    ArrayAlloc { len: VarId },
-    // Array field read
+
+    /// Array allocation
+    ArrayAlloc {
+        len: VarId,
+    },
+
+    /// Array field read
     ArrayGet(VarId, VarId),
-    // Array field write
+
+    /// Array field write
     ArrayPut(VarId, VarId, VarId),
 }
 
@@ -93,10 +129,16 @@ pub enum Atom {
     Var(VarId),
 }
 
-// Exit nodes of basic blocks
+/// A basic block exit node
 #[derive(Debug, PartialEq, Eq)]
 pub enum Exit {
+    /// Function return: return the value of the given variable
     Return(VarId),
+
+    /// Conditional jump: compare `v1` with `v2` according to `cond`, jump to `then_block` if true,
+    /// jump to `else_block` otherwise.
     Branch { v1: VarId, v2: VarId, cond: Cmp, then_block: BlockIdx, else_block: BlockIdx },
+
+    /// Unconditional jump
     Jump(BlockIdx),
 }
